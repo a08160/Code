08aa8bc9d60bf858985a1a0634f569a727a8987a
a08160 <a08160@g.skku.edu>
2025-01-06 18:23:53 +0900

    commit]

--- python/basic_4.py
+++ python/basic_4.py
@@ -107,3 +107,19 @@ a.pop()
 
 x = ["q", "w", "e", "r", "w"]
 print(x.count("w"))
+
+
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+
+# 1. 2번 인덱스 값 출력
+print(rainbow.index(2))
+
+# 2. 알파벳 순서로 정렬한 값 출력하기
+print(rainbow.sort)
+
+# 3. 좋아하는 색 맨 마지막에 추가하기
+rainbow.append("green")
+
+# 4. 3~6번째 값 삭제하기
+
+del rainbow[3:7]
